5bc85f97bae32e5dc49a3983adca528f5a9ab3bf
Reza Nazari <persian1developers@gmail.com>
2020-04-11 19:14:36 +0430

    Just Do It.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { View, Text } from "react-native";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(0,0,0,.8)",
+      }}
+    >
+      <Text style={{ color: "white", fontSize: 40 }}>Hello, World!</Text>
+    </View>
   );
 }
 
